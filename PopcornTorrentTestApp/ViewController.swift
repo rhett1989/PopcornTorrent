3add19e4f72e26e9c1620586a10d1fe9bf6929f0
Buzzel <yogi.bear.development@gmail.com>
2016-04-04 15:08:17 -0700

    Add a test app and stop downloading the torrent once it completes

---
+++ PopcornTorrentTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopcornTorrentTestApp
+//
+//  Created by Anthony Castelli on 4/3/16.
+//  Copyright © 2016 PopcornTime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
